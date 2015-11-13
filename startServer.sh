1227a34e3b43538b8f08a3eb45d2404150e45773
Eric Williamson <ericrw96@vt.edu>
2015-11-13 08:47:49 -0500

    adding in shebang

--- startServer.sh
+++ startServer.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #Starts the server with forever so that we can have it restart
 echo "Starting the Server with forever.js";
 type forever >/dev/null 2>&1 || { echo "I require forever but it's not installed.  Attempting to install it ."; sudo npm install -g forever; }
