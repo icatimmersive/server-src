4dda7c3ba47fb10acf4d7e3f9587693174b99fc9
Eric Williamson <ericrw96@vt.edu>
2015-11-13 09:05:13 -0500

    adding in path exporting for the forever command

--- startServer.sh
+++ startServer.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export PATH="./node_modules/.bin:$PATH";
 #Starts the server with forever so that we can have it restart
 echo "Starting the Server with forever.js";
 type forever >/dev/null 2>&1 || { echo "I require forever but it's not installed.  Attempting to install it ."; sudo npm install -g forever; }
--- stopServer.sh
+++ stopServer.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+export PATH="./node_modules/.bin:$PATH"
 forever stopall
