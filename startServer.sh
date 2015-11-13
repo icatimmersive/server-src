b374a1f98fd76b1a192a7fdd0a56c38b3c0438cf
Eric Williamson <ericrw96@vt.edu>
2015-11-13 08:37:01 -0500

    add in bash scripts to enable forever on the server

---
+++ startServer.sh
@@ -0,0 +1,6 @@
+#Starts the server with forever so that we can have it restart
+echo "Starting the Server with forever.js";
+type forever >/dev/null 2>&1 || { echo "I require forever but it's not installed.  Attempting to install it ."; sudo npm install -g forever; }
+forever start server.js;
+echo "If this failed make sure to install forever and have it in your path"
+echo "npm install -g forever";
