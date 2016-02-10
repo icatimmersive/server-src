da9df462a0cd2bdf980ee02f63da95d462df5b7a
Eric Williamson <ericrw96@vt.edu>
2016-02-10 12:49:07 -0500

    add in documentation of room subscription to examples

--- Examples/Simple_Listeners/JavaScript/simple_listener.js
+++ Examples/Simple_Listeners/JavaScript/simple_listener.js
@@ -3,8 +3,14 @@
  * from their node-js library.
  */
 // Need the Socket.io client library from npm listed in the package.json
-var socket = require('socket.io-client')('http://localhost');
-socket.emit('start', {connectionType: 'LISTENER'}); //We are telling the server we will listen for blobs
+var socket = require('socket.io-client')('http://localhost:8888');
+socket.emit('start', {connectionType: 'LISTENER'}); //We are telling the server we will listen for
+//all blobs
+/*
+ If we wanted to listen for specific camera ids we need to populate a reqCameras field with an array of ids
+ Example:
+ socket.emit('start', {connectionType: 'LISTENER', reqCameras: [0,1,2]}); //will subscript to cameras 0,1,and 2
+ */
 
 socket.on('connect', function () {
     console.log('connected to server!');
@@ -19,7 +25,7 @@ socket.on('newBlob', function (blob) {
 });
 
 socket.on('updateBlob', function (blob) {
-    //console.log('updateBlob');
+    console.log('updateBlob' + JSON.stringify(blob));
     //logging update blobs may spit out too much to the console to be readable so it is commented out for now
 });
 
